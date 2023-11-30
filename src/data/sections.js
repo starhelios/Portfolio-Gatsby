aec6fcf16379081080bcd5d82826850e031e4a57
starhelios <starhelios94@gmail.com>
2023-11-30 16:53:52 -0600

    remove unused sidebar image

--- src/data/sections.js
+++ src/data/sections.js
@@ -1,6 +1,5 @@
 import {
   FaDev,
-  FaSignLanguage,
   GoTools,
   IoIosDocument,
   IoIosPaperPlane,
@@ -15,7 +14,6 @@ const sections = [
   { id: "education", title: "Education", icon: MdSchool },
   { id: "skills", title: "Skills", icon: GoTools },
   { id: "projects", title: "Projects", icon: FaDev },
-  { id: "languages", title: "Languages", icon: FaSignLanguage },
   { id: "resume", title: "Resume", icon: IoIosDocument },
   { id: "contact", title: "Contact", icon: IoIosPaperPlane },
 ];
